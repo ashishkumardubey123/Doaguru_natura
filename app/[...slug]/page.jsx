1a9c13dbdeb91ee06d6645a7ed6a05057885daee
umerqureshifine <ashish9039062705@gmail.com>
2026-03-17 12:18:18 +0530

    convert next.config.ts to js and remodes some fils related to ts

--- app/[...slug]/page.jsx
+++ app/[...slug]/page.jsx
@@ -1,5 +1,9 @@
 import { redirect } from "next/navigation";
 
+export function generateStaticParams() {
+  return [];  // Empty array - koi static page generate nahi hoga
+}
+
 export default function CatchAllRedirect() {
   redirect("/");
 }
