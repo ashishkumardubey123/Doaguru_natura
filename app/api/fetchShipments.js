184ccd3d9c7ea8d1e39ced6c9175c536e6a8ea42
Ashish Kumar <ashish9039062705@gmail.com>
2026-05-08 16:48:33 +0530

    fix: update API_BASE_URL to use production endpoint and comment out localhost

--- app/api/fetchShipments.js
+++ app/api/fetchShipments.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = "http://localhost:5000";
+// const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = "https://natura-backend-xct1.onrender.com"
 
 export const fetchAllShipments = async (page = 1, limit = 50) => {
   try {
