ae592c60cb6538a5387b6ce25a8fc6819c79d1dd
Ashish Kumar <ashish9039062705@gmail.com>
2026-05-08 18:25:27 +0530

    fix: comment out local base URL and set production API_BASE_URL for country coordinates fetch

--- app/utils/countryCoordinates.js
+++ app/utils/countryCoordinates.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 export const fetchAllCountryCoordinates = async () => {
   try {
     // Apne Node.js backend ka sahi URL yahan dalein
-     const baseUrl = `http://localhost:5000`
+    //  const baseUrl = `http://localhost:5000`
+    const API_BASE_URL = "https://natura-backend-xct1.onrender.com"
     const response = await axios.get(`${baseUrl}/api/contry/AllCountries`);
     
     if (response.data.success) {
